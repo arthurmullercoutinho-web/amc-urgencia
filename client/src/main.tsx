b5e2660a149e0a8cc701596695f3c4c3b61369d3
arthurmullercoutinho-web <arthurmullercoutinho@gmail.com>
2026-06-12 13:57:25 -0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,38 @@
 import { createRoot } from "react-dom/client";
+import { useEffect } from "react";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
+function Analytics() {
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-VRPMKC203K";
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-VRPMKC203K');
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
+  return null;
+}
+
+createRoot(document.getElementById("root")!).render(
+  <>
+    <Analytics />
+    <App />
+  </>
+);
